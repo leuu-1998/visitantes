7a559aefdd6135cab4802bb19196ea542dbf5446
leuu-1998 <lmoinah@uni.pe>
2022-02-11 23:23:10 -0500

    i did added head and body to the table

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(
 );
 
 app.get("/", async (req, res) => {
-  // se recibe el query string
+  //  query string recived
   var { name } = req.query;
 
   if ((name !== undefined) & (name !== "")) {
@@ -28,7 +28,6 @@ app.get("/", async (req, res) => {
       { name: name },
       { $inc: { count: 1 } }
     );
-    //modifiedcount=1 => se actualizó ==0 entonces no
     //if not update a visitor then we have to create a new visitor
     if (data.modifiedCount === 0) {
       try {
@@ -49,25 +48,24 @@ app.get("/", async (req, res) => {
   // then get all the collection
   const visitors = await Visitor.find({});
   res.send(
-    `<div>
+    `
       <table>
-        <tr>
-          <td><strong>Id</strong></td>
-          <td><strong>Name</strong></td>
-          <td><strong>Visits</strong></td>
-        </tr>
-        ${visitors.map(
-          (oneVisitor) =>
-            `<tr><td>${oneVisitor._id}</td><td>${oneVisitor.name}</td><td>${oneVisitor.count}</td></tr>`
-        )}
+        <thead>
+          <tr>
+            <th><strong>Id</strong></th>
+            <th><strong>Name</strong></th>
+            <th><strong>Visits</strong></th>
+          </tr>
+        </thead>
+        <tbody>
+          ${visitors.map(
+            (oneVisitor) =>
+              `<tr><td>${oneVisitor._id}</td><td>${oneVisitor.name}</td><td>${oneVisitor.count}</td></tr>`
+          )}
+        </tbody>
       </table>
-    </div>
-`
+    `
   );
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!!!"));
-
-/* res.send(
-  
-); */
